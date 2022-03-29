b4347f761f7152918cd894ac4111969b9e3d506d
hidekazoo <tanakahidek@gmail.com>
2022-03-29 20:46:15 +0900

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,44 @@
+import type { LoaderFunction } from "remix";
+import { useLoaderData } from "remix";
+export let loader: LoaderFunction = async ({ context }) => {
+  const { SAMPLE_VARIABLE } = context;
+  return {
+    variable: SAMPLE_VARIABLE,
+  };
+};
+
+export default function Index() {
+  const data = useLoaderData();
+  console.log(data);
+
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
